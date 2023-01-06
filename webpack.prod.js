db112e40ed87c837c2af9af7294f445401a71ff0
mariomui <yi.kan.mui@gmail.com>
2023-01-06 06:25:33 -0800

    Create seperate packs for analyze and prod.

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,39 +5,40 @@ const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = /** @type { import('webpack').Configuration } */ ({
-  entry: "./lib/index.ts",
-  mode: "production",
-  optimization: {
-    minimize: true,
-  },
-  target: "node",
-  module: {
-    rules: [
-      {
-        test: /\.[tj]s?$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      // {
-      //   test: /\.ts?$/,
-      //   use: "ts-loader",
-      //   exclude: /node_modules/,
-      // },
+module.exports = (env, argv) =>
+  /** @type { import('webpack').Configuration } */ ({
+    entry: "./lib/index.ts",
+    mode: "production",
+    optimization: {
+      minimize: true,
+    },
+    target: "node",
+    module: {
+      rules: [
+        {
+          test: /\.[tj]s?$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        // {
+        //   test: /\.ts?$/,
+        //   use: "ts-loader",
+        //   exclude: /node_modules/,
+        // },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    output: {
+      filename: "server.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: env.analyzerMode || "none",
+        openAnalyzer: !!env.analyzerMode,
+      }),
+      new CleanWebpackPlugin(),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    filename: "server.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
-    }),
-    new CleanWebpackPlugin(),
-  ],
-});
+  });
